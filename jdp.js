37d591657d5eb4805fb159eded2dc9652fe42267
cryptoskillz <cryptoskillz@protonmail.com>
2020-02-09 13:29:19 +0700

    first

--- jdp.js
+++ jdp.js
@@ -1,47 +1,48 @@
 /*
 
-jdp (javascript data processor) is lighteight dom parser that will take object and replace dom elements that
+JDP (javascript data processor) is lightweight Dom parser that will take object and replace Dom elements that
 match the data in the page.
 
 It is inspired by riot.js, knockout.js and nunjucks with the following differences
 
-It is super light weight, it processes the dom and replaces vars (that is it)
-It has the template data in the same page as the HTML as having a second file to hold it is an uncessary overhead
-It supports the nunjucks syntax and a subset of its command {{var}} {%for %} {%if%} {%else%} to offer clinet side handlebars processing
+It is super light weight, it processes the Dom and replaces vars (that is it)
+It has the template data in the same page as the HTML as having a second file to hold it is an unnecessary overhead
+It supports the nunjucks syntax and a subset of its command {{var}} {%for %} {%if%} {%else%} to offer client side handlebars processing
 
 The use case is.  
 
 1) Add the vars you want replace to the HTML file
 2) Make an AJAX call get
-3) process the dom and replace vars with the data in the ajax result
+3) process the Dom and replace vars with the data in the ajax result
 
 
-todo
+to-do
 process for
 process if
 process else
 
 */
 var jdp = (function() {
-    //get the dom elements
+    //get the Dom elements
     var elems = document.body.getElementsByTagName("*");
     //this function checks to see if the element contains a reserved word if, else, for
+    //note: this function is very much in Dev IE it does not work yet
     function checkElement(elementObj) {
         //create the patterns;
         var regexBrackets = new RegExp('\{%([^)]+)\%}');
-        //note: for some reasomn this regExp searching for "for" is not working so using the index of for now
+        //note: for some reason this regExp searching for "for" is not working so using the index of for now
         var regexFor = new RegExp('\b(\w*for\w*)\b');
         //look for a bracket {% to %}
         var isBracket = regexBrackets.test(elementObj.innerText);
         if (isBracket == true) {
             //now we found a bracket lets see if we can match it against a reserved word
-            //todo: we can use a regex to search for this a little more elegantly.
+            //to-do: we can use a regex to search for this a little more elegantly.
             var isFor = elementObj.innerText.indexOf('for');
             //debug
             //console.log("isFor");
             //console.log(isFor);
             if (isFor == -1) {
-                //todo: not for so check for an if
+                //to-do: not for so check for an if
             } else {
                 //it is a for so process it.
                 //console.log(elementObj.innerText)
@@ -75,7 +76,7 @@ var jdp = (function() {
             }
         }
     }
-    //this funciton process the element and puts it into an object
+    //this function process the element and puts it into an object
     function processElement(theElement) {
         //get the object
         var obj = {}
